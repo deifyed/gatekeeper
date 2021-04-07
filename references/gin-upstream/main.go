1b77230bbe3330eea6ed068c2b2b1d8f6066be20
Julius Pedersen <deifyed@tutanota.com>
2021-04-07 17:57:35 +0200

    ✅ Add reference upstream

--- references/gin-upstream/main.go
+++ references/gin-upstream/main.go
@@ -12,20 +12,13 @@ package main
 import (
 	"log"
 
-	// WARNING!
-	// Change this to a fully-qualified import path
-	// once you place this file into your project.
-	// For example,
-	//
-	//sw "github.com/GIT_USER_ID/GIT_REPO_ID/go"
-	//
-	sw "./"
+	"github.com/deifyed/gin-upstream/pkg/core"
 )
 
 func main() {
 	log.Printf("Server started")
 
-	router := sw.NewRouter()
+	router := core.NewRouter()
 
-	log.Fatal(router.Run(":8080"))
+	log.Fatal(router.Run(":3000"))
 }
